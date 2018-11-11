87ed3a20095e1823b8f49d7e2463b2225a4e5f33
maxCut <maxCut@github.com>
2018-11-10 21:06:17 -0500

    base game framework

--- Game.java
+++ Game.java
@@ -1,11 +1,18 @@
 import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
-import javax.event.MouseEvent;
-import javax.event.MouseMotionAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.Graphics;
 public class Game extends JComponent
 {
     static int mouseX;
     static int mouseY;
+    
+    static BufferedImage Background;
+
     static Pet momo = new Pet();
     public Game()
     {
@@ -21,18 +28,27 @@ public class Game extends JComponent
     }
     public static void main(String[] args)
     {
+        try
+        {
+            Background = ImageIO.read(new File("./GameBackground.jpg"));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error loading image");
+        }
         JFrame frame = new JFrame();
         Game M = new Game();
         frame.add(M);
-        frame.setSize(3000,3000);
-        frame.setVisable(true);
+        frame.setSize(930,700);
+        frame.setVisible(true);
         while(true)
         {
 
         }
     }
-    public void paintComponent()
+    public void paintComponent(Graphics g)
     {
-
+        g.drawImage(Background,0,0,null);
+        momo.draw(g);
     }
 }
