e703cf2e6199ba04c41ff61446547f4ca7949c2d
Sumara Ali <sumaraali523@gmail.com>
2018-11-11 00:38:12 -0500

    All kirby states done

--- Pet.java
+++ Pet.java
@@ -24,9 +24,11 @@ public class Pet
 	public BufferedImage[] angry;
 	public BufferedImage[] idle;
 	public BufferedImage[] happy;
+	public BufferedImage[] eating;
+	public BufferedImage[] jumping;
 	public BufferedImage currentImage; 
 	private int counter = 0;
-	
+	private int delay = 0;
 	public Pet(){
 		hunger = 10.0;
 		thirst = 10.0;
@@ -44,11 +46,39 @@ public class Pet
 		}
 		
 		angry = new BufferedImage[2];
-		for(int i =0; i< 2; i++) {
-			angry[i] = img.getSubimage(1891 +(i*(width)), 1250 + height, width, height);
+		angry[0] = img.getSubimage(1891 , 1250 + height, width, height);
+		angry[1] = img.getSubimage(1891 + width+12, 1250 + height, width, height);
+		
+		happy = new BufferedImage[3];
+		for(int i =0; i< 3; i++) {
+			happy[i] = img.getSubimage(1891 +(i*(width)), 1250 + 300, width, height);
 		}
 		
-		currentImage = angry[0];
+		idle = new BufferedImage[3];
+		idle[0] = img.getSubimage(15, 0, width, height);
+		idle[1] = img.getSubimage(9 + width, 0, width, height);
+		idle[2] = img.getSubimage(3 + 2*width, 0, width, height);
+		
+		eating = new BufferedImage[3];
+		eating[0] = img.getSubimage(5, 465, width, height);
+		eating[1] = img.getSubimage(width, 465, width, height);
+		eating[2] = img.getSubimage(2*width, 465, width, height);
+		
+		jumping = new BufferedImage[9];
+		jumping[0] = img.getSubimage(5 , 370, width, height);
+		jumping[1] = img.getSubimage(5 + width , 370, width, height);
+		jumping[2] = img.getSubimage(10 + 2*width, 370, width, height);
+		jumping[3] = img.getSubimage(15+ 3*width, 370, width, height);
+		jumping[4] = img.getSubimage(5+4*width, 370, width, height);
+		jumping[5] = img.getSubimage(-1+ 5*width, 370, width, height);
+		jumping[6] = img.getSubimage(-1+ 6*width, 370, width, height);
+		jumping[7] = img.getSubimage( -4+7*width, 370, width, height);
+		jumping[8] = img.getSubimage(-2+8*width, 370, width, height);
+		
+		BufferedImage dead = img.getSubimage(675, 825, width +5, height);
+		currentImage = dead;
+		//currentImage = sad[0];
+		
 		
 		
 		
@@ -63,15 +93,17 @@ public class Pet
 		hunger = hunger - decrement;
 		thirst = thirst - decrement;
 		mood = mood - decrement;
-		updateCurrentImage();
+		//updateCurrentImage();
 	}
 	public void updateCurrentImage() {
-		boolean forever = true;
-		counter ++;
-		currentImage = sad[counter];
-		if (counter == 2) {
-				counter = 0;
-			}
+		delay++;
+		if(delay % 25 ==0) {
+			currentImage = jumping[counter];
+			counter ++;
+			if (counter == 9) {
+					counter = 0;
+				}
+		}
 		
 		
 	}
