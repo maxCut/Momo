a51ec3ef24601098e0dec131118df777c8492651
maxCut <maxCut@github.com>
2018-11-10 22:57:36 -0500

    added food

--- Drink.java
+++ Drink.java
@@ -3,26 +3,26 @@ import java.io.File;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 
-public class Food
+public class Drink 
 {
     final int width = 60;
     final int height= 60;
-    final int locationX = 100;
+    final int locationX = 600;
     final int locationY = 100;
 
-    static BufferedImage FoodImage;
-    public Food()
+    static BufferedImage DrinkImage;
+    public Drink()
     {
         try
         {
-            FoodImage = ImageIO.read(new File("./food.png"));
+            DrinkImage = ImageIO.read(new File("./drink.png"));
         }
         catch(Exception e)
         {
             System.out.println("Error loading image");
         }
     }
-    public boolean isFeeding(int x,int y)
+    public boolean isWatering(int x,int y)
     {
         return (x>locationX && x<locationX + width && y>locationY && y<locationY+height);
     }
@@ -30,11 +30,11 @@ public class Food
     {
         if(feeding)
         {
-            g.drawImage(FoodImage,x-width/2,y-height/2,width,height,null);
+            g.drawImage(DrinkImage,x-width/2,y-height/2,width,height,null);
         }
         else
         {
-            g.drawImage(FoodImage,locationX,locationY,width,height,null);
+            g.drawImage(DrinkImage,locationX,locationY,width,height,null);
         }
     }
 }
